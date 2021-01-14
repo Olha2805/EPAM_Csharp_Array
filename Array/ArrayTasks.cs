9f5e452d3fc724dda7ab3b02ead0df728d72cfc3
Olha Bohatova <torshynina@gmail.com>
2021-01-14 17:29:34 +0000

    homework complete

--- Array/ArrayTasks.cs
+++ Array/ArrayTasks.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ArrayObject
 {
@@ -9,8 +9,20 @@ namespace ArrayObject
         /// </summary>
         public static void ChangeElementsInArray(int[] nums)
         {
-            // TODO: delete code line below, write down your solution 
-            // Hint: Don`t create or initialize any array, work only with given 'numbs'
+            int firsr_number = 0;
+            int second_number = nums.Length - 1;
+    
+            for (int count = nums.Length / 2; count > 0; count--, firsr_number++, second_number--)
+            {
+                int x = nums[firsr_number];                
+                int y = nums[second_number];             
+                if (nums[firsr_number] % 2 == 0 && nums[second_number] % 2 == 0) {               
+                    nums[firsr_number] = nums[firsr_number] + nums[second_number];
+                    nums[second_number] = nums[firsr_number] - nums[second_number]; 
+                    nums[firsr_number] = nums[firsr_number] - nums[second_number];                 
+                }
+            }
+            return nums;
             throw new NotImplementedException();
         }
 
@@ -18,9 +30,22 @@ namespace ArrayObject
         /// Task 2
         /// </summary>
         public static int DistanceBetweenFirstAndLastOccurrenceOfMaxValue(int[] nums)
-        {
-            // TODO: delete code line below, write down your solution 
-            // Hint: Don`t modify array 'nums'
+        {           
+            int max = nums[0];
+            int biggest_value_position = 0;
+            int second_value_position = 0;
+            int gap = 0;
+            for (int position = 1; position < nums.Length; position++)
+            {
+                if (nums[position] > max)
+                {
+                    max = nums[position];
+                    biggest_value_position = position;
+                }
+                if (nums[position] == max) second_value_position = position;
+            }
+            gap = second_value_position - biggest_value_position;
+            return gap;
             throw new NotImplementedException();
         }
 
@@ -29,8 +54,16 @@ namespace ArrayObject
         /// </summary>
         public static void ChangeMatrixDiagonally(int[,] matrix)
         {
-            // TODO: delete code line below, write down your solution 
-            // Hint: Don`t create or initialize array, work only with given 'matrix'
+            for (int i = 0; i < Math.Sqrt(matrix.Length); i++)
+            {
+              for (int j = 0; j < Math.Sqrt(matrix.Length); j++)
+                {
+                    if (i < j) matrix[i, j] = 0;
+                    else if (i > j) matrix[i, j] = 1;
+                }
+               
+            }
+            return matrix;
             throw new NotImplementedException();
         }
     }
