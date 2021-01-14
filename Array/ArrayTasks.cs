252434d03046cf6334645ef366b4f1b581aca5f4
Olha Bohatova <torshynina@gmail.com>
2021-01-14 17:40:07 +0000

    1

--- Array/ArrayTasks.cs
+++ Array/ArrayTasks.cs
@@ -62,7 +62,7 @@ namespace ArrayObject
                 }
                
             }
-            return matrix;
+            
             throw new NotImplementedException();
         }
     }
